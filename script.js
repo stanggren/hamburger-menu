0ac1a58ff41ee41be8f696313d3b379226d987ab
stanggren <astanggren@gmail.com>
2020-10-23 14:25:53 +0200

    fontSize func

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const header = document.getElementById("header");
 const hamburger = document.getElementById("hamburger-content")
-
+const pList = document.querySelectorAll("p");
 toggleHamburger = () => {
     // header.style.height = '100vh';
     header.classList.toggle('hamburger');
     hamburger.classList.toggle('display-hamburger-content');
+}
+
+enlargeFontSize = () => {
+    for (let i = 0; i < pList.length; i++){
+        console.log(pList[i]);
+        let computedFontSize = window.getComputedStyle(pList[i]).fontSize;
+        let px = computedFontSize.substring(0, computedFontSize.length - 2);
+        pList[i].style.fontSize = px * 1.2 + "px"; 
+    }
 }
