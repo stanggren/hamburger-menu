f1c99c23d362e27e7e5fd7dffa1f68019bb841e9
stanggren <astanggren@gmail.com>
2020-10-23 13:39:06 +0200

    added burger menu

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const header = document.getElementById("header");
-
+const hamburger = document.getElementById("hamburger-content")
 
 toggleHamburger = () => {
     // header.style.height = '100vh';
     header.classList.toggle('hamburger');
+    hamburger.classList.toggle('display-hamburger-content');
 }
